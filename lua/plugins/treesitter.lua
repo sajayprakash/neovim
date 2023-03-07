83f7ffbf14ba43c470ddcbe1ede99bd0590d81a2
Sajay Prakash <sajayprakash7@protonmail.com>
2023-03-07 18:56:13 +0530

    add basic lazy-vim config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "tsx",
+        "typescript",
+        "c",
+        "java",
+      })
+    end,
+  },
+}
